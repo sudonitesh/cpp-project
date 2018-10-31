eca0f381a52e519f548fbaf6dd23f2b389faf154
b117020 <debangshumehta@gmail.com>
2018-10-31 21:01:46 +0530

    Gostel class

--- include/classes.h
+++ include/classes.h
@@ -1,13 +1,51 @@
 #ifndef CLASSES_H
 #define CLASSES_H
 
-class Demo
+// class Demo
+// {
+//     private:
+//         int i;
+//     public:
+//         Demo(int n);
+//         int showData();
+// };
+class Hostel
 {
-    private:
-        int i;
     public:
-        Demo(int n);
-        int showData();
+        vector<int>rnumber;
+        vector<int>rfloor;
+        vector<int> studentid;
+        vector<string>studentname;
+        vector<int>studentroom;
+
+        void program_open();
+
+        void header();
+        
+        // int idposition(int sid); // for update, delete , search
+
+        void add_student();
+        
+
+        void stud_del();
+
+        void get_stud_info();
+        void stud_all_display();
+        void stud_search();
+        void stud_clear(); //remove all students
+
+        void get_roominfo();
+        // void room_add();
+        void room_search();
+        // void room_remove();
+        // int room_position(int n); //get floor
+        int check_room(int roomid);
+        void available_rooms();
+
+        
+
+
+        void program_close();
 };
 
 #endif
