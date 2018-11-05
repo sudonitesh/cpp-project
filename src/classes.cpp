8b47f6913387ed3a0e9b78467d5200bf67a471f7
itsmepiyush2 <piyushmishra1999@gmail.com>
2018-11-05 21:08:16 +0530

    Added some Hostel fns

--- src/classes.cpp
+++ src/classes.cpp
@@ -84,3 +84,72 @@ void Hostel::available_rooms()
 {
     //Addition of code after add_student() function
 }
+
+//display all student details
+void Hostel::stud_all_display()
+{
+    cout<< "Student ID\tStudent Name\tRoom No."<< endl;
+    for(unsigned int i = 0; i <= studentname.size(); i++)
+    {
+        cout<< studentid[i]<<"\t"<< studentname[i]<<"\t"<<rnumber[i]<< enndl;;
+    }
+}
+
+//Add a student
+void Hostel::add_student()
+{
+    string id;
+    cout<<"Enter student id: ";
+    cin>> id;
+    string name;
+    cout<<"Enter student name: ";
+    cin>> name;
+    if(rnumber.size() > 60)
+    {
+        cout<< "No vacancy."<< endl;
+    }
+    else
+    {
+        int i = 0;
+        while(rnumber[i] != NULL)
+        {
+            i++;
+        }
+        studentname[i] = name;
+        studentid[i] = id;
+        rnumber[i] = i;
+        if(rnumber[i] <= 20) //20 rooms in one floor
+            rfloor[i] = 1;
+        else if(rnumber[i] > 20 && rnumber[i] <= 40)
+            rfloor[i] = 2;
+        else
+            rfloor[i] = 3;
+    }
+    display_details(i);
+}
+
+//Display the details of a given index
+void Hostel::display_details(int i)
+{
+    cout<< "ID: "<< studentid[i]<< endl;
+    cout<< "Name: "<< studentname[i]<< endl;
+    cout<<"Room No.: "<< rnumber[i]<< endl;
+    cout<<"Floor No.: "<< rfloor[i]<< endl;
+}
+
+//Remove a student
+void Hostel::stud_del()
+{
+    int room_no;
+    cout<< "Enter room number: ";
+    cin>> room_no;
+    int i = 0;
+    while(rnumber[i] != room_no)
+    {
+        i++;
+    }
+    rnumber[i] = NULL;
+    studentname[i] = NULL;
+    studentid[i] = NULL;
+    rfloor[i] = NULL;
+}
