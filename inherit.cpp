b3f3882e5a09328c8bfece8c7086c669c098da20
MetalDent <37808619+MetalDent@users.noreply.github.com>
2018-11-12 02:24:49 +0530

    Update inherit.cpp

--- inherit.cpp
+++ inherit.cpp
@@ -52,15 +52,36 @@ class Student: public Hostel
 {
     public:
     void header(); // functions that student can access
+//////////////////////////////////////////FOR ALL THE BELOW THREE FUNCTIONS, TELL ME IF YOU WANT TO ADD THREE DISPLAY FUNCTIONS FOR EACH////////////////////
+    void leave_app(string ID, string name, string room_no, string start_date, string end_date, string reason) // 1. leave application
+    {
+	string file_name = "../database/leave.csv", line;
+	ofstream file(file_name, ios::out);
 
-    // 1. leave application.. user input id, name, message, room_no
-    // store in db
+	line = ID + "," + name + "," + room_no + "," + start_date + "," + end_date + "," + reason + "\n";
+	file<<line;
+	file.close();
+    }
     
-    // 2. if student wants to join then get id, and details, send to db
+    void join(string ID, string name) // 2. if student wants to join then get id, and details, send to db
+    {
+	string file_name = "../database/join.csv", line;
+	ofstream file(file_name, ios::out);
 
-    // 3. application request for guest... room reserved for guest 
-    
+	line = ID + "," + name + "\n";
+	file<<line;
+	file.close();
+    }
 
+    void guest(string ID, string name, string start_date, string end_date, string guest_name, string guest_relation) // 3. application request for guest... room reserved for guest 
+    {
+	string file_name = "../database/guest.csv", line;
+	ofstream file(file_name, ios::out);
+
+	line = ID + "," + name + "," + start_date + "," + end_date + "," + guest_name + "," + guest_relation + "\n";
+	file<<line;
+	file.close();
+    }
 };
 
 class Admin: public Hostel
@@ -229,6 +250,50 @@ void Admin::stud_clear()
     studentname.clear();
 }
 
+
+void vector_2_file()	//stores values from all the three vectors into db1.csv
+{
+	string file_name = "../database/db1.csv", line;
+
+	ofstream file(file_name, ios::out);
+
+	for(int i = 0 ; i < nm_v.size() ; i++)
+	{
+		line = id_v[i] + "," + nm_v[i] + "," + rm_v[i] + "\n";
+		file<<line;
+	}
+
+}
+
+void file_display()	//displays db1.csv
+{
+		
+	string file_name = "../database/db1.csv", delimeter = ",";
+
+	ifstream file(file_name);
+	vector<vector<string>> dataList;
+	string line = "";
+
+	while(getline(file, line))
+	{
+		vector<string> vect;
+		boost::algorithm::split(vect, line, boost::is_any_of(delimeter));
+		dataList.push_back(vect);
+	}
+
+	for(vector<string> vect : dataList)
+	{
+		for(string data : vect)
+		{
+			cout<<data<<" , ";
+		}
+
+		cout<<endl;
+	}
+
+	file.close();
+
+}
 
 
 int main()
