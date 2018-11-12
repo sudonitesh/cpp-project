60170357cbe47690a045604528ee33965b090b97
Nitesh Tosniwal <nktosni@gmail.com>
2018-11-12 21:41:01 +0530

    file structure changed

---
+++ __works/src/functions.cpp
@@ -0,0 +1,64 @@
+#include "../include/functions.h"
+#include <iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<boost/algorithm/string.hpp>
+
+using namespace std;
+
+int fooFun()
+{
+    double a=100;
+    return a;
+}
+
+vector<vector<string>> program_open()	//I don't think you need this now 
+{
+	string file_name = "../database/db.csv", delimeter = ",";
+
+	ifstream file(file_name);
+	vector<vector<string>> dataList;
+	string line = "";
+
+	while(getline(file, line))
+	{
+		vector<string> vect;
+		boost::algorithm::split(vect, line, boost::is_any_of(delimeter));
+		dataList.push_back(vect);
+	}
+
+	return dataList;
+}
+
+// vector<vector<string>> store_value()	//stores all IDs, names and room nos in separate vectors
+// {
+// 	string file_name = "../database/db.csv", delimeter = ",";
+
+// 	ifstream file(file_name);
+	
+// 	vector<string> nm_v, rm_v, id_v;
+
+// 	string line = "";
+
+// 	getline(file, line);
+
+// 	while(getline(file, line))
+// 	{
+// 			vector<string> vect;
+// 			boost::algorithm::split(vect, line, boost::is_any_of(delimeter));
+			
+// 			id_v.push_back(vect[0]);
+// 			nm_v.push_back(vect[1]);
+// 			rm_v.push_back(vect[2]);
+// 	}
+
+// 	file.close();
+
+// 	for (int i = 0 ; i < 60 ; i++)	//display all the vectors (delete if you don't want)
+//   	{
+// 	   cout <<  id_v[i] << " " << nm_v[i] << " " << rm_v[i] << endl;
+//   	}
+// }
--- include/classes.h
+++ include/classes.h
@@ -3,52 +3,60 @@
 #include <vector>
 #include<string>
 
-
-// class Demo
-// {
-//     private:
-//         int i;
-//     public:
-//         Demo(int n);
-//         int showData();
-// };
 class Hostel
 {
     public:
         std::vector<std::string>rnumber;
-        std::vector<int>rfloor;
         std::vector<std::string> studentid;
         std::vector<std::string>studentname;
         std::vector<int>studentroom;
-
-        void program_open();
-
-        void header();
         
-        // int idposition(int sid); // for update, delete , search
-
-        void add_student();
-        void store_value();
-
-        void stud_del();
-
-        void get_stud_info();
+        
+        void header();
+        void store_value(); //*
+        
+        void vector_2_file();
+        void file_display();
+        void name_search();
+        
+    
         void stud_all_display();
         void stud_search();
-        void stud_clear(); //remove all students
-
-        void get_roominfo();
-        // void room_add();
+    
         void room_search();
-        // void room_remove();
-        // int room_position(int n); //get floor
-        int check_room(int roomid);
-        void available_rooms();
 
+        void check_room();
+        void vector_to_array();
         
-
-
-        void program_close();
+        
+        
+        void stud_del(); //*(done)
+        
+        void program_close();//* file close
 };
 
+class Student: public Hostel
+{
+    private:
+        string password;
+    public:
+        void header();
+        void leave_app();
+        
+        void join() ;
+        
+        void guest();
+};
+
+class Admin: public Hostel
+{
+private:
+    string password;
+
+public:
+    void header();
+            
+    void add_student();
+    void stud_clear(); 
+};
 #endif
