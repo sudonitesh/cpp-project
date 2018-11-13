5f0c5920b90b4d154f9941b3ca34b9cd01e8d053
smitap-31 <41297528+smitap-31@users.noreply.github.com>
2018-11-13 08:41:33 +0530

    added print statements

--- src/main.cpp
+++ src/main.cpp
@@ -18,81 +18,91 @@ int main()
     H.store_value();
     int choice=1;
     do{
-        cout<<"\nPress the appropriate key:"<<endl;
-        cout<<"\n1. Admin\n2. Student\n0. Exit\n";
+        cout<<"\n\t\t\t~~-----------------------------~~";
+        cout<<"\n\t\t\t  **PRESS the appropriate key**"<<endl;
+        cout<<"\t\t\t~~-----------------------------~~"<<endl;
+        cout<<"\n\t\t^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+        cout<<"\n\t\t\t1. Admin\n\t\t\t2. Student\n\t\t\t0. Exit\n\n\t\t\tEnter your choice:";
         int admin_choice=1, stud_choice;
         cin>>choice;
-        
+        cout<<endl;
+
             if(choice == 1)
             {
                 A.header();
                 H.header();
 
+                cout<<"\n\t\t\tEnter your choice:";
                 cin>> admin_choice;
+                cout<<endl;
+
                 switch(admin_choice)
                 {
-                    case 1: 
+                    case 1:
                         A.add_student();
                         break;
-                    case 2: 
+                    case 2:
                         H.stud_del();
                         break;
-                    case 3: 
+                    case 3:
                         A.stud_clear(); //!error
                         break;
-                    case 4: 
+                    case 4:
                         H.name_search();
                         break;
-                    case 5: 
+                    case 5:
                         H.stud_search();
                         break;
-                    case 6: 
+                    case 6:
                         H.check_room();
                         break;
-                    case 7: 
+                    case 7:
                         H.room_search();
-                        break; 
-                    case 8: 
+                        break;
+                    case 8:
                         H.stud_all_display();
                         break;
                 }
             }
-            
+
             else if(choice == 2)
             {
                 S.header();
                 H.header();
+
+                cout<<"\n\t\t\tEnter your choice:";
                 cin>> stud_choice;
+                cout<<endl;
+
                 switch(stud_choice)
                 {
-                    case 1: 
+                    case 1:
                         S.leave_app();
                         break;
-                    case 2: 
+                    case 2:
                         S.join();
                         break;
-                    case 3: 
+                    case 3:
                         S.guest();
                         break;
-                    case 4: 
+                    case 4:
                         H.name_search();
                         break;
-                    case 5: 
+                    case 5:
                         H.stud_search();
                         break;
-                    case 6: 
+                    case 6:
                         H.check_room(); //!error
                         break;
-                    case 7: 
+                    case 7:
                         H.room_search();
-                        break; 
-                    case 8: 
-                        H.stud_all_display();
+                    case 8:
+                        H.stud_all_display(); //!error
                         break;
                 }
             }
     } while(choice!=0);
-    
+
     return 0;
 }
 
